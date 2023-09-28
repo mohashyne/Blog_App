e918fc08cb6ff4c5d7e2b0b9a0d86655eee669be
MSA <crystalbluetech@gmail.com>
2023-09-28 20:39:17 +0100

    add custom methods to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  def author=(author)
-    self.user = author
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,18 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "User"
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :update_post_counter
+
+  # Public Methods
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.update(post_counter: author.posts.count)
+  end
 end
