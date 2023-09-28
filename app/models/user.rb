c329e77f34ff4ac59922100db1330a7a474998e8
MSA <crystalbluetech@gmail.com>
2023-09-28 21:14:32 +0100

    add validation for user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # custom method
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
