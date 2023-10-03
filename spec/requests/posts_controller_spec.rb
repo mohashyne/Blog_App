65b9a22af924b4b62a477944405bded5490350fa
MSA <crystalbluetech@gmail.com>
2023-10-03 16:02:37 +0100

    updat post controller spec

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,24 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
-  let(:user) { create(:user) }
-  let(:post) { create(:post, author: user) }
+RSpec.describe 'Posts', type: :request do
+  # Crear un usuario para las pruebas
+  let(:user) { FactoryBot.create(:user) }
 
-  describe 'GET #index' do
-    it 'assigns @posts and renders the index template' do
-      get :index, params: { user_id: user.id }
+  describe 'GET /user/:user_id/posts' do
+    it 'returns a successful response' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to have_http_status(200)
+    end
 
-      expect(assigns(:posts)).to eq(user.posts)
+    it 'renders the index template' do
+      get user_posts_path(user_id: user.id)
       expect(response).to render_template(:index)
+    end
+
+    it 'displays the correct placeholder text in the response body' do
+      get user_posts_path(user_id: user.id)
+      expect(response.body).to include('All Posts')
     end
   end
 
-  describe 'GET #show' do
-    it 'assigns @post and renders the show template' do
-      get :show, params: { id: post.id }
+  describe 'GET /user/:user_id/posts/:id' do
+    it 'returns a successful response' do
+      post = FactoryBot.create(:post, author: user)
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(200)
+    end
 
-      expect(assigns(:post)).to eq(post)
+    it 'renders the show template' do
+      post = FactoryBot.create(:post, author: user)
+      get user_post_path(user_id: user.id, id: post.id)
       expect(response).to render_template(:show)
+    end
+
+    it 'displays the correct placeholder text in the response body' do
+      post = FactoryBot.create(:post, author: user)
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response.body).to include('Total Comments')
     end
   end
 end
