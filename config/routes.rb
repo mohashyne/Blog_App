5f1b78ce5987a46dc7c35e20dad3e5102aae41a0
MSA <crystalbluetech@gmail.com>
2023-10-06 14:56:39 +0100

    add comments and likes helper

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,16 @@ Rails.application.routes.draw do
 
   # root "articles#index"
   get '/users/:id', to: 'users#show', as: 'user'
-
   get '/user/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
   get '/user/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+  get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
+  post '/users/:user_id/posts', to: 'posts#create', as: 'user_posts_create'
+
+  get '/users/:user_id/posts/:post_id/comments/new', to: 'comments#new', as: 'new_user_post_comment'
+  post '/users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'user_post_comments'
+
+  post '/user/:user_id/posts/:post_id/likes', to: 'likes#create', as: 'user_post_likes'
 
   resources :posts, only: [:show]
 end
